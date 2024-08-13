3738d0671f4aef7bc1742e51cb77dfcceaa02e18
etesh-1322 <58354302+etesh-1322@users.noreply.github.com>
2024-08-13 20:54:28 +0530

    Enhanced test methods of MarketingFormTest.java

--- src/test/java/TestCases/MarketingFormTest.java
+++ src/test/java/TestCases/MarketingFormTest.java
@@ -10,23 +10,18 @@ public class MarketingFormTest extends BaseClass {
 
 
     @Test(description = "This test will fill the form and verify the error message when user submit the form without adding message")
-    public void verifyMarketingForm() throws InterruptedException {
+    public void verifyFillingTheMarketingForm() throws InterruptedException {
+
+        String firstName = "Test_user";
+        String Org_name = "Tendable";
+        String Number = "7492013421";
+        String email = "test@gmail.com";
         System.out.println("Driver: " + driver);
+
         MarketingPageFunctions MarketingPageForm = new MarketingPageFunctions(driver);
         Assert.assertTrue(MarketingPageForm.clickContactUSButton());
         Assert.assertTrue(MarketingPageForm.clickMarketingFormButton());
-        Assert.assertTrue(MarketingPageForm.EnterfirstName());
-        Assert.assertTrue(MarketingPageForm.EnterOrganizationName());
-        Assert.assertTrue(MarketingPageForm.EnterPhoneNumber());
-        Assert.assertTrue(MarketingPageForm.EnterEmail());
-        Assert.assertTrue(MarketingPageForm.selectConsentRadioButton());
-        Assert.assertTrue(MarketingPageForm.clickSubmitButton());
-        Assert.assertTrue(MarketingPageForm.errorMessage()); // Here we will verify the error message when message is not added while submiting the form
-
-
-
-
-
+        Assert.assertTrue(MarketingPageForm.submitFormAndVerify(firstName,Org_name,Number,email));
 
     }
 
